9dfe8216d175436d20adf2d607e1f41ee90c1053
Yuliya Osmolovskaya <yuliya.asmalouskaya@gmail.com>
2020-08-31 12:50:33 +0200

    Add menu tabs

---
+++ src/js/menu-tabs.js
@@ -0,0 +1,18 @@
+export const menuTab = () => {
+    const tabs = document.querySelectorAll('[data-tab-target]');
+    const tabContents = document.querySelectorAll('[data-tab-content]');
+    console.log(tabs);
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.tabTarget);
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active')
+            });
+            tabs.forEach(tab => {
+                tab.classList.remove('active')
+            });
+            tab.classList.add('active');
+            target.classList.add('active');
+        });
+    })
+}
